78590ece2d16c58ce5aa3b9532b53f289be44bff
elijah <elijahndenwa19@gmail.com>
2023-07-27 14:13:01 +0100

    Your commit message here

---
+++ frontend/react-admin-ui/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>EimpactChart</span>
+      <span>© EimpactChart</span>
+    </div>
+  )
+}
+
+export default Footer
